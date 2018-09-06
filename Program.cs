50404548517fd55174e67f11b3c999de48236767
Jince <jince1991@163.com>
2018-09-06 18:25:25 +0800

    no message

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace LeetCodeTest
     {
         static void Main(string[] args)
         {
-            ISolution solution = new FindKthLargest();
+            ISolution solution = new MinimumTotal();
             solution.Test();
         }
     }
@@ -385,7 +385,7 @@ namespace LeetCodeTest
             }
             return nums[k - 1];
         }
-        
+
         //快排
         private int Solution2(int[] nums, int k)
         {
@@ -426,6 +426,276 @@ namespace LeetCodeTest
         {
             int[] nums = new int[6] { 3, 2, 1, 5, 6, 4 };
             Console.WriteLine(Solution2(nums, 2));
+            Console.ReadLine();
+        }
+    }
+    #endregion
+
+    #region 最长上升子序列
+
+    /// <summary>
+    /// https://leetcode-cn.com/problems/longest-increasing-subsequence/description/
+    /// 2018.9.4
+    /// </summary>
+    public class LengthOfLIS : ISolution
+    {
+        /* 
+         * 动态规划：
+         * 1.最优子结构
+         * 2.边界
+         * 3.状态转移方程
+         */
+        /// <summary>
+        /// 以每一个元素为结尾的最长子序列，等于其前面的比其小的最长子序列加1
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        private int Solution(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+            int[] l = new int[nums.Length];
+            int max = 0;
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                l[i] = 1;
+                for (int j = 0; j < i; ++j)
+                {
+                    if (nums[i] >= nums[j] && l[i] < l[j] + 1)
+                        l[i] = l[j] + 1;
+                }
+                if (l[i] > max)
+                    max = l[i];
+            }
+            return max;
+        }
+
+        public void Test()
+        {
+            int[] nums = new int[6] { 10, 9, 2, 5, 3, 4 };
+            Console.WriteLine(Solution(nums));
+            Console.ReadLine();
+        }
+    }
+    #endregion
+
+    #region  最大子序和
+
+    /// <summary>
+    /// https://leetcode-cn.com/problems/maximum-subarray/description/
+    /// 2018.9.5
+    /// </summary>
+    public class MaxSubArray : ISolution
+    {
+        private int Solution(int[] nums)
+        {
+            if (nums == null)
+                throw new Exception("Array is null");
+            int last = nums[0];
+            int max = nums[0];
+            for (int i = 1; i < nums.Length; ++i)
+            {
+                int sum = last + nums[i];
+                last = nums[i];
+                if (sum > nums[i])
+                    last = sum;
+
+                if (last > max)
+                    max = last;
+            }
+            return max;
+        }
+
+        public void Test()
+        {
+            int[] nums = new int[2] { -2, 1 };
+            Console.WriteLine(Solution(nums));
+            Console.ReadLine();
+        }
+    }
+
+    #endregion
+
+    #region 不同路径
+    /// <summary>
+    /// https://leetcode-cn.com/problems/unique-paths/description/
+    /// 2018.9.5
+    /// </summary>
+    public class UniquePaths : ISolution
+    {
+        private int Solution(int m, int n)
+        {
+            if (m <= 0 || n <= 0)
+                throw new Exception("Number error");
+            int[,] paths = new int[m, n];
+            for (int i = 0; i < m; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (i == 0 || j == 0)
+                        paths[i, j] = 1;
+                    else
+                        paths[i, j] = paths[i - 1, j] + paths[i, j - 1];
+                }
+            }
+            return paths[m - 1, n - 1];
+        }
+
+        public void Test()
+        {
+            Console.WriteLine(Solution(7, 3));
+            Console.ReadLine();
+        }
+    }
+    #endregion
+
+    #region 不同路径 II
+    /// <summary>
+    /// https://leetcode-cn.com/problems/unique-paths-ii/description/
+    /// 2018.9.6
+    /// </summary>
+    public class UniquePathsWithObstacles : ISolution
+    {
+        private int Solution(int[,] obstacleGrid)
+        {
+            if (obstacleGrid == null || obstacleGrid.Length == 0)
+                throw new Exception("Data error");
+            if (obstacleGrid[0, 0] == 1)
+                return 0;
+
+            int m = obstacleGrid.GetLength(0);
+            int n = obstacleGrid.GetLength(1);
+            int[,] paths = new int[m, n];
+            paths[0, 0] = 1;
+            for (int i = 0; i < m; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (i == 0 && j == 0)
+                        continue;
+
+                    if (obstacleGrid[i, j] == 1)
+                    {
+                        paths[i, j] = 0;
+                        continue;
+                    }
+                    if (i == 0)
+                        paths[i, j] = paths[i, j - 1] == 0 ? 0 : 1;
+                    else if (j == 0)
+                        paths[i, j] = paths[i - 1, j] == 0 ? 0 : 1;
+                    else
+                        paths[i, j] = paths[i - 1, j] + paths[i, j - 1];
+                }
+            }
+            return paths[m - 1, n - 1];
+        }
+
+        public void Test()
+        {
+            //             int[,] obstacleGrid = new int[1, 2]
+            //                 {
+            //                     {0,1}
+            //                 };
+            int[,] obstacleGrid = new int[3, 3]
+                {
+                    { 0, 0, 0 },{0, 1, 0 },{0, 0, 0 }
+                };
+            Console.WriteLine(Solution(obstacleGrid));
+            Console.ReadLine();
+        }
+    }
+    #endregion
+
+    #region 三角形最小路径和
+    /// <summary>
+    /// https://leetcode-cn.com/problems/triangle/description/
+    /// 2018.9.6
+    /// </summary>
+    public class MinimumTotal : ISolution
+    {
+        /// <summary>
+        /// 动态规划，通过一个有两行的二维数组暂存结果，计算当前行依赖于上一行的结果，从上而下依次交替计算
+        /// </summary>
+        /// <param name="triangle"></param>
+        /// <returns></returns>
+        private int Solution1(List<List<int>> triangle)
+        {
+            if (triangle == null || triangle.Count == 0)
+                throw new Exception("Triangle error");
+
+            int longest = triangle[triangle.Count - 1].Count;
+            int[,] sums = new int[2, longest];
+            sums[0, 0] = triangle[0][0];
+            for (int i = 1; i < triangle.Count; ++i)
+            {
+                var row = triangle[i];
+                int current = i % 2;
+                int last = (i + 1) % 2;
+                for (int j = 0; j < row.Count; ++j)
+                {
+                    if (j == 0)
+                    {
+                        sums[current, j] = sums[last, 0] + row[j];
+                    }
+                    else if (j == row.Count - 1)
+                    {
+                        sums[current, j] = sums[last, row.Count - 2] + row[j];
+                    }
+                    else
+                    {
+                        if (sums[last, j - 1] > sums[last, j])
+                            sums[current, j] = sums[last, j] + row[j];
+                        else
+                            sums[current, j] = sums[last, j - 1] + row[j];
+                    }
+                }
+            }
+            int idx = (triangle.Count - 1) % 2;
+            int min = sums[idx, 0];
+            for (int i = 1; i < longest; ++i)
+            {
+                if (sums[idx, i] < min)
+                    min = sums[idx, i];
+            }
+            return min;
+        }
+
+        /// <summary>
+        /// 动态规划，倒序寻找路径。先复制最后一行，从倒数第二行开始计算
+        /// </summary>
+        /// <param name="triangle"></param>
+        /// <returns></returns>
+        private int Solution2(List<List<int>> triangle)
+        {
+            if (triangle == null || triangle.Count == 0)
+                throw new Exception("Triangle error");
+
+            int[] sums = new int[triangle.Count];
+            var lastLine = triangle[triangle.Count - 1];
+            for (int i = 0; i < triangle.Count; ++i)
+            {
+                sums[i] = lastLine[i];
+            }
+
+            for (int i = triangle.Count - 2; i > -1; --i)
+            {
+                for (int j = 0; j <= i; ++j)
+                {
+                    sums[j] = (sums[j] < sums[j + 1] ? sums[j] : sums[j + 1]) + triangle[i][j];
+                }
+            }
+            return sums[0];
+        }
+
+        public void Test()
+        {
+            //int[] nums = new int[10] { 2, 3, 4, 6, 5, 7, 4, 1, 8, 3 };
+            List<List<int>> triangle = new List<List<int>>();
+            triangle.Add(new List<int>() { 2 });
+            triangle.Add(new List<int>() { 3, 4 });
+            triangle.Add(new List<int>() { 6, 5, 7 });
+            triangle.Add(new List<int>() { 4, 1, 8, 3 });
+            Console.WriteLine(Solution2(triangle));
             Console.ReadLine();
         }
     }
